09b1724a427ce1f4f7c7ac12e582511a6d978ef5
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-12-23 11:07:44 +0300

    23.12.2022 - Uygulama 9 yapıldı.

--- app/src/main/java/com/example/kararvedonguunite4/Uyg9Activity.java
+++ app/src/main/java/com/example/kararvedonguunite4/Uyg9Activity.java
@@ -9,34 +9,49 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Uyg9Activity extends AppCompatActivity {
-    EditText txtNumbers;
+    EditText txtNumbers, txtNumber;
+    int number;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.uyg9_activity);
         txtNumbers = findViewById(R.id.txtNumbers);
+        txtNumber = findViewById(R.id.txtNumber);
     }
 
     public void btnDoubleNumbers(View view) {
+        String str = "";
         txtNumbers.setText("");
-        for (int i = 0; i < 15; i += 2) {
-            if(i % 2 == 0) txtNumbers.setText(txtNumbers.getText().toString() + " " + i);
+        number = Integer.parseInt(txtNumber.getText().toString());
+        for (int i = 0; i < number; i++) {
+            if(i % 2 == 0) str += txtNumbers.getText().toString() + i + " - ";
         }
+        str = str.substring(0, str.length() - 3);
+        txtNumbers.setText(str);
+
     }
 
     public void btnSingleNumbers(View view) {
+        String str = "";
         txtNumbers.setText("");
-        for (int i = 0; i < 15; i++) {
-            if(i % 2 != 0) txtNumbers.setText(txtNumbers.getText().toString() + " " + i);
+        number = Integer.parseInt(txtNumber.getText().toString());
+        for (int i = 0; i < number; i++) {
+            if(i % 2 != 0) str += txtNumbers.getText().toString() + i + " - ";
         }
+        str = str.substring(0, str.length() - 3);
+        txtNumbers.setText(str);
     }
 
     public void btnAllNumbers(View view) {
+        String str = "";
         txtNumbers.setText("");
-        for (int i = 0; i < 15; i++) {
-            txtNumbers.setText(txtNumbers.getText().toString() + " " + i);
+        number = Integer.parseInt(txtNumber.getText().toString());
+        for (int i = 0; i < number; i++) {
+            str += txtNumbers.getText().toString() + i + " - ";
         }
+        str = str.substring(0, str.length() - 3);
+        txtNumbers.setText(str);
     }
 
     public void btnBack(View view) {

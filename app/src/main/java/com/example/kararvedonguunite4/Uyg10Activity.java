896c3c3dcec6bc91b4390a13a4de407856dc16cf
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-12-23 11:07:03 +0300

    23.12.2022 - Uygulama 10 Yapıldı

--- app/src/main/java/com/example/kararvedonguunite4/Uyg10Activity.java
+++ app/src/main/java/com/example/kararvedonguunite4/Uyg10Activity.java
@@ -2,16 +2,41 @@ package com.example.kararvedonguunite4;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Uyg10Activity extends AppCompatActivity {
+    EditText txtNumber;
+    TextView txtResult;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.uyg10_activity);
+        txtNumber = findViewById(R.id.txtNumber);
+        txtResult = findViewById(R.id.txtResult);
+        Log.v("Etiket", "saaaaaas");
+    }
+
+    public void btnSuccess(View view) {
+        Log.v("Etiket", "sa");
+        int number = Integer.parseInt(txtNumber.getText().toString());
+        Log.v("Etiket", "as");
+        int counter = 1;
+        Log.v("Etiket", "saa");
+        long result = 1;
+        Log.v("Etiket", "ass");
+        while(counter <= number) {
+            result = result * counter;
+            counter++;
+        }
+        Log.v("Etiket", "saaa");
+        txtResult.setText("Sonuç: " + result);
     }
 
     public void btnBack(View view) {
